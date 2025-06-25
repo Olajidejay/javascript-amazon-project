90b116377d7ff070262edd94a11a97483bf4b280
Olajidejay <jamalbaba84@gmail.com>
2025-06-25 22:48:13 +0100

    Move add to cart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart (productId) {
+    let matchingItem; 
+
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+       matchingItem = cartItem;
+      } 
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1; // Increment quantity if item already exists  
+    } else {
+      cart.push({
+      productId: productId,
+      quantity: 1 // Default quantity, can be adjusted later
+    });
+    }
+}
